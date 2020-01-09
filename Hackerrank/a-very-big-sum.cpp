69bd1c60413c61e2d0b57ac50cdfde30e89fb585
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-01-09 09:31:50 +0530

    A-very-big-sum hackerrank added

---
+++ Hackerrank/a-very-big-sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    long sum=0;
+    for(int i=0;i<n;i++){
+        long f;
+        cin>>f;
+        sum=sum+f;
+    }
+    cout<<sum;
+}
+    
+
