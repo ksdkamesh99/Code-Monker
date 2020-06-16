30f7f942cad0cac28d07baa2ceb0fd060f658d0e
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-06-16 18:48:10 +0530

    #709.cpp leetcode file added

---
+++ LeetCode/#709.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for(int i=0;i<str.length();i++){
+            str[i]=tolower(str[i]);
+        }
+        return str;
+    }
+};
