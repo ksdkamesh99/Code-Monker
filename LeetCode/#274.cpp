187d5f603d7e8775b5910d4aef40bfbe3fd1c13e
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-06-25 13:03:54 +0530

    #274.cpp leetcode file added

---
+++ LeetCode/#274.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end());
+        int l=0;
+        int n=citations.size();
+        int r=n-1;
+        int m;
+        int ans=0;
+        while(l<=r){
+            m=l+(r-l)/2;
+            if(citations[m]>=(n-m)){
+                r=m-1;
+                ans=n-m;
+            }
+            else{
+                l=m+1;
+            }
+        }
+        return ans;
+    }
+};
