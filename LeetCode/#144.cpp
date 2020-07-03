6120d9c55da498a3c689f2a21ac91a714e0b9814
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-07-03 19:17:37 +0530

    Leetcode #144.cpp added

---
+++ LeetCode/#144.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+    void preorder(TreeNode *root,vector<int> &v){
+        if(root==NULL){
+            return;
+        }
+        v.push_back(root->val);
+
+        preorder(root->left,v);
+
+        preorder(root->right,v);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>v;
+        preorder(root,v);
+        return v;
+    }
+};
+    
